41fd25e3c8f4ff28c8fcb99e9c20ef861b1165c2
Keano Porcaro <porkd002@mymail.unisa.edu.au>
2017-11-14 22:04:16 +1030

    Third iteration

--- src/App.js
+++ src/App.js
@@ -20,21 +20,54 @@ function App() {
       </div>
     </div>
     <div id="header-text">
-      <h1>POKEMON</h1>
+      <h1 id="pokemonName">POKEMON</h1>
     </div>
-    <div id="pokemon-stats">
-      <p id="pokemonID" class="stats"><font size="1">NO</font><br/>
+    <div id="pokemon-details">
+      <p id="pokemonID" class="details"><font size="1">NO</font><br/>
         #ID
       </p>
-      <p id="pokemonHeight" class="stats"><font size="1">HEIGHT</font><br/>
+      <p id="pokemonHeight" class="details"><font size="1">HEIGHT</font><br/>
         0.00m
       </p>
-      <p id="pokemonWeight" class="stats"><font size="1">WEIGHT</font><br/>
+      <p id="pokemonWeight" class="details"><font size="1">WEIGHT</font><br/>
         00.0kg
       </p>
-      <p id="pokemonType" class="stats"><font size="1">TYPE</font><br/>
+      <p id="pokemonType" class="details"><font size="1">TYPE</font><br/>
         Water
       </p>
+    </div>
+    <div id="pokemon-stats">
+      <p>BASE STATS</p>
+      <hr/>
+      <div class="segment">
+        <label for="HP" class="baseStats">HP</label><br/><br/>
+        <div id="HP" class="baseStats2">600</div>
+      </div>
+      <div class="divider" />
+      <div class="segment">
+        <label for="Attack" class="baseStats">ATTACK</label><br/><br/>
+        <div id="Attack" class="baseStats2">600</div>
+      </div>
+      <div class="divider" />
+      <div class="segment">
+        <label for="Defense" class="baseStats">DEFENSE</label><br/><br/>
+        <div id="Defense" class="baseStats2">600</div>
+      </div>
+      <div class="divider" />
+      <div class="segment">
+        <label for="SPAttack" class="baseStats">SP.ATT</label><br/><br/>
+        <div id="SPAttack" class="baseStats2">600</div>
+      </div>
+      <div class="divider" />
+      <div class="segment">
+        <label for="SPDefense" class="baseStats">SP.DEF</label><br/><br/>
+        <div id="SPDefense" class="baseStats2">600</div>
+      </div>
+      <div class="divider" />
+      <div class="segment">
+        <label for="Speed" class="baseStats">SPEED</label><br/><br/>
+        <div id="Speed" class="baseStats2">600</div>
+      </div>
     </div>
     </div>
     </div>
